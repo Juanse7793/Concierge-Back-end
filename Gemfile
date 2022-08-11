ac3a45b767b08c52af3a4ed6165db1483933cd6a
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-08-11 10:37:44 -0500

    mvc for concierge

--- Gemfile
+++ Gemfile
@@ -9,8 +9,8 @@ gem 'rails', '~> 7.0.3', '>= 7.0.3.1'
 # Rubocop
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+
+gem 'pg', '~> 1.1'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  resources :users do 
+    resources :reservations
+  end
+
+  resources :events do 
+    resources :reservations
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
