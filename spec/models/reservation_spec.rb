3bc3919c720c73a8989ed2f56ffa7dc54bf086ca
tafaramafemba <tafara.maf@gmail.com>
2022-08-12 18:52:08 +0200

    add request_specs for API endpoints

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Event, type: :model do
     @event = Event.create(name: "tomorrowland", location: "USA", price: 1000,
       start_date: "09/12/2022", end_date: "12/12/2022", id: 1)
 
-    @user = User.create(name: "Juan", id: 1)
+    @user = User.create(name: "Juan", id: 28)
 
     @reservation = Reservation.create(date: "10/12/2022", city: "Barcelona", user_id: @user.id, event_id: @event.id)
   end
