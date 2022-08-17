1b639301e517a1a67187441b2df94901b6766f68
tafaramafemba <tafara.maf@gmail.com>
2022-08-17 18:58:15 +0200

    permit image

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -49,7 +49,7 @@ module Api
 
       # Only allow a list of trusted parameters through.
       def event_params
-        params.permit(:name, :price, :start_date, :end_date, :location)
+        params.permit(:name, :price, :start_date, :end_date, :location, :image)
       end
     end
   end
