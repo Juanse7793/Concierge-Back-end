5c47c33b4445e546671feba9505eccbac9c1af28
tafaramafemba <tafara.maf@gmail.com>
2022-08-17 18:56:48 +0200

    add dependent destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   has_one_attached :image
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   validates :name, presence: true
   validates :location, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
