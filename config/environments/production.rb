070efef65d4c2dab6a55a21295a5688949e1296c
tafaramafemba <tafara.maf@gmail.com>
2022-08-19 22:22:47 +0200

    add default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,10 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options = {
+  host: 'https://ancient-lake-57758.herokuapp.com'
+}
+
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
