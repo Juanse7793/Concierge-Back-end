337c1cdbace7f22b23cd107f3f00ed62e7d38826
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-08-23 18:12:47 -0500

    gemfile-lock8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.routes.default_url_options = {
-  host: 'https://arcane-island-84220.herokuapp.com'
+  host: 'https://blooming-springs-95964.herokuapp.com'
 }
 
 
