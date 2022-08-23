7dacb0c96a7cdaa5d1605646cc37ae18a57eabfa
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-08-23 15:33:25 -0500

    root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
-  root :to => 'api#v1#users'
+  mount Rswag::Api::Engine => '/api-docs', :as => 'swagger'
+  root :to => 'home#index'
 
   namespace :api do
     namespace :v1 do
